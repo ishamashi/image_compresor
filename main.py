6704e43b9679d6b4ee91cd0376aa3c8405f9c71c
Icho Elkabron <icho.ishamashi@gmail.com>
2024-11-09 12:49:37 +0800

    Add

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image
+
+def convert_to_webp(input_dir, quality=80):
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                file_path = os.path.join(root, file)
+                output_file = os.path.splitext(file_path)[0] + '.webp'
+
+                try:
+                    img = Image.open(file_path)
+                    
+                    img.save(output_file, 'webp', quality=quality)
+                    print(f"Converted {file_path} to {output_file}")
+                except Exception as e:
+                    print(f"Error converting {file_path}: {e}")
+
+if __name__ == "__main__":
+    public_folder = "./public"  
+    convert_to_webp(public_folder, quality=60) 
